f34dd5d795424bdd854a8df278c5b4229babb19b
pavelhardzei <pavelgordei11@gmail.com>
2022-06-24 11:15:40 +0300

    added save and delete in database mixin

---
+++ services/api/base/database/mixins.py
@@ -0,0 +1,13 @@
+from api.base.database.config import SessionLocal
+
+
+class SaveDeleteDBMixin:
+    def save(self):
+        with SessionLocal() as session:
+            session.add(self)
+            session.commit()
+
+    def delete(self):
+        with SessionLocal() as session:
+            session.delete(self)
+            session.commit()
