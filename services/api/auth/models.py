1011b986d5051aa88a9e87ade2eff24ba6f337bd
pavelhardzei <pavelgordei11@gmail.com>
2022-06-24 11:07:02 +0300

    restructured database subpackage

--- services/api/auth/models.py
+++ services/api/auth/models.py
@@ -1,6 +1,7 @@
 import enum
 
-from base.database import Base
+from auth.utils import pwd_context
+from base.database.config import Base
 from sqlalchemy import Boolean, Column, Enum, Integer, String
 
 
@@ -17,6 +18,13 @@ class UserProfile(Base):
     email = Column(String(30), unique=True, nullable=False)
     is_active = Column(Boolean, default=False)
     role = Column(Enum(Role), default=Role.viewer)
+    password = Column(String(72), nullable=False)
+
+    def set_password(self, plain_password):
+        self.password = pwd_context.hash(plain_password)
+
+    def check_password(self, plain_password):
+        return pwd_context.verify(plain_password, self.password)
 
     def __repr__(self):
         return f'UserProfile(id={self.id}, email={self.email}, is_active={self.is_active})'
