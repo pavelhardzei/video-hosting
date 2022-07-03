8503f43e23b532d8e22053ad93f3b423cfdb44c7
pavelhardzei <pavelgordei11@gmail.com>
2022-07-04 00:49:39 +0300

    moved session to property

--- services/api/base/database/mixins.py
+++ services/api/base/database/mixins.py
@@ -3,12 +3,16 @@ from base.database.config import SessionLocal
 
 class SaveDeleteDBMixin:
     def save(self):
-        with SessionLocal() as session:
+        with self.session() as session:
             session.add(self)
             session.commit()
             session.refresh(self)
 
     def delete(self):
-        with SessionLocal() as session:
+        with self.session() as session:
             session.delete(self)
             session.commit()
+
+    @property
+    def session(self):
+        return SessionLocal
