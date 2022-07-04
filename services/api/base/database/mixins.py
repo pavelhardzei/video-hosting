3285f4f150858d5ca9e6930b746ce470d164bf05
pavelhardzei <pavelgordei11@gmail.com>
2022-07-04 11:39:34 +0300

    moved session to class attribute

--- services/api/base/database/mixins.py
+++ services/api/base/database/mixins.py
@@ -2,17 +2,19 @@ from base.database.config import SessionLocal
 
 
 class SaveDeleteDBMixin:
+    _session_class = SessionLocal
+
     def save(self):
-        with self.session() as session:
+        with self.session_class() as session:
             session.add(self)
             session.commit()
             session.refresh(self)
 
     def delete(self):
-        with self.session() as session:
+        with self.session_class() as session:
             session.delete(self)
             session.commit()
 
     @property
-    def session(self):
-        return SessionLocal
+    def session_class(self):
+        return self._session_class
