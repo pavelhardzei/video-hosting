1de844957fb18190b13dff4bc911eb2788e3d462
pavelhardzei <pavelgordei11@gmail.com>
2022-07-04 11:41:55 +0300

    written signup test

--- services/api/testing/auth/tests.py
+++ services/api/testing/auth/tests.py
@@ -1,6 +1,22 @@
+from unittest.mock import ANY
+
 from auth.models import UserProfile
+from fastapi import status
 from testing.conftest import client
 
 
 def test_signup(session):
-    assert session.query(UserProfile).count() == 0
+    response = client.post('/api/v1/auth/signup/', json={'email': 'test@test.com',
+                                                         'username': 'test',
+                                                         'password': 'testing321'})
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json() == {'id': ANY,
+                               'email': 'test@test.com',
+                               'username': 'test',
+                               'is_active': True,
+                               'role': UserProfile.RoleEnum.viewer}
+
+    assert session.query(UserProfile).count() == 1
+    user = session.query(UserProfile).first()
+    assert user.check_password('testing321')
