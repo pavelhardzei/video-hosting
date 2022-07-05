9fcad806bb36b1a08b8b87dfc42626e4eb70589f
pavelhardzei <pavelgordei11@gmail.com>
2022-07-05 13:01:51 +0300

    fixed exp claim

--- services/api/auth/utils.py
+++ services/api/auth/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from base.settings import settings
 from jose import jwt
 from passlib.context import CryptContext
@@ -10,7 +12,7 @@ def create_access_token(data: dict):
     '''For access token creating'''
 
     to_encode = data.copy()
-    to_encode.update({"exp": settings.access_token_expire_minutes})
+    to_encode.update({'exp': datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
 
     return encoded_jwt
