0989b0753fa037b93baa0ee9c56891fcac03867e
pavelhardzei <pavelgordei11@gmail.com>
2022-07-05 11:57:51 +0300

    added util for access token creating

--- services/api/auth/utils.py
+++ services/api/auth/utils.py
@@ -1,3 +1,19 @@
+from datetime import datetime, timedelta
+
+from base.settings import settings
+from jose import jwt
 from passlib.context import CryptContext
 
+# for plain password encryption
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+# for access token creating
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+
+    return encoded_jwt
