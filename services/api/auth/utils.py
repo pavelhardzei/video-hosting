b77809a971a187bb951ac8d147edd206115bb6d8
pavelhardzei <pavelgordei11@gmail.com>
2022-07-05 12:32:31 +0300

    passed expire_minutes directly

--- services/api/auth/utils.py
+++ services/api/auth/utils.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 from base.settings import settings
 from jose import jwt
 from passlib.context import CryptContext
@@ -8,12 +6,11 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-def create_access_token(data: dict, expires_delta: timedelta):
+def create_access_token(data: dict):
     '''For access token creating'''
 
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": settings.access_token_expire_minutes})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
 
     return encoded_jwt
