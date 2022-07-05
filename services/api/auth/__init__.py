9e1910eb6f2108254ad1235f1330c7623f92ec48
pavelhardzei <pavelgordei11@gmail.com>
2022-07-05 15:09:04 +0300

    added JWTError handling

--- services/api/auth/__init__.py
+++ services/api/auth/__init__.py
@@ -3,20 +3,26 @@ from base.database.dependencies import session_dependency
 from base.settings import settings
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/v1/auth/signin/')
 
 
 def current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(session_dependency)):
-    payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
-    user_id = payload['id']
+    token_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                    detail='Could not validate credentials',
+                                    headers={'WWW-Authenticate': 'Bearer'})
 
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+    except JWTError:
+        raise token_exception
+
+    user_id = payload.get('id')
     user = session.query(UserProfile).filter(UserProfile.id == user_id).first()
+
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Could not validate credentials',
-                            headers={'WWW-Authenticate': 'Bearer'})
+        raise token_exception
 
     return user
