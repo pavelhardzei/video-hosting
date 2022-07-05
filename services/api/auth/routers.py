c7f21e898813b47668db2ce6373a037f8508d3a6
pavelhardzei <pavelgordei11@gmail.com>
2022-07-05 14:21:58 +0300

    added signin endpoint

--- services/api/auth/routers.py
+++ services/api/auth/routers.py
@@ -1,6 +1,11 @@
+from auth import current_user
 from auth.models import UserProfile
-from auth.schemas import UserProfileCreateSchema, UserProfileSchema
-from fastapi import APIRouter, status
+from auth.schemas import TokenSchema, UserProfileCreateSchema, UserProfileSchema
+from auth.utils import create_access_token
+from base.database.dependencies import session_dependency
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix='/auth',
@@ -18,3 +23,21 @@ def signup(user: UserProfileCreateSchema):
     user_model.save()
 
     return user_model
+
+
+@router.post('/signin/', response_model=TokenSchema)
+def signin(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(session_dependency)):
+    user = session.query(UserProfile).filter(UserProfile.email == form_data.username).first()
+
+    if user is None or not user.check_password(form_data.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Incorrect email or password',
+                            headers={'WWW-Authenticate': 'Bearer'})
+    access_token = create_access_token({'id': user.id})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.get('/users/me/', response_model=UserProfileSchema)
+def get_user(user: UserProfile = Depends(current_user)):
+    return user
