6397828964309f0e797103f099a865cffee47598
pavelhardzei <pavelgordei11@gmail.com>
2022-07-06 17:18:56 +0300

    added patch user endpoint

--- services/api/auth/users/routers.py
+++ services/api/auth/users/routers.py
@@ -1,6 +1,7 @@
 from auth.dependencies import current_user
 from auth.models import UserProfile
-from auth.schemas import UserProfileSchema
+from auth.schemas import UserProfileSchema, UserProfileUpdateSchema
+from base.database import crud
 from fastapi import APIRouter, Depends
 
 router = APIRouter(
@@ -10,4 +11,11 @@ router = APIRouter(
 
 @router.get('/', response_model=UserProfileSchema)
 def get_user(user: UserProfile = Depends(current_user)):
+    return user
+
+
+@router.patch('/', response_model=UserProfileSchema)
+def patch_user(data: UserProfileUpdateSchema, user: UserProfile = Depends(current_user)):
+    crud.update(user, data)
+
     return user
