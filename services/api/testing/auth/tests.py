740251be033ea5ce382d2b4684464ffef48f099e
pavelhardzei <pavelgordei11@gmail.com>
2022-07-06 11:44:56 +0300

    added test for signin and get_current_user

--- services/api/testing/auth/tests.py
+++ services/api/testing/auth/tests.py
@@ -1,8 +1,9 @@
 from unittest.mock import ANY
 
 from auth.models import UserProfile
+from auth.utils import create_access_token
 from fastapi import status
-from testing.conftest import client
+from testing import client
 
 
 def test_signup(session):
@@ -20,3 +21,42 @@ def test_signup(session):
     assert session.query(UserProfile).count() == 1
     user = session.query(UserProfile).first()
     assert user.check_password('testing321')
+
+
+def test_signin(user):
+    response = client.post('/api/v1/auth/signin/', data={'username': 'test@test.com',
+                                                         'password': 'testing321'})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'access_token': ANY, 'token_type': 'bearer'}
+
+
+def test_signin_invalid_credentials(session, user):
+    response = client.post('/api/v1/auth/signin/', data={'username': 'fake@fake.com',
+                                                         'password': 'testing321'})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    response = client.post('/api/v1/auth/signin/', data={'username': 'test@test.com',
+                                                         'password': 'fake_password'})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_get_current_user(user_token):
+    response = client.get('/api/v1/auth/users/me/', headers={'Authorization': f'Bearer {user_token}'})
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'id': ANY,
+                               'email': 'test@test.com',
+                               'username': 'test',
+                               'is_active': True,
+                               'role': UserProfile.RoleEnum.viewer}
+
+
+def test_get_current_user_invalid_token(user_token):
+    response = client.get('/api/v1/auth/users/me/', headers={'Authorization': f'Bearer {user_token}_fake'})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Signature verification failed.'}
+
+    response = client.get('/api/v1/auth/users/me/', headers={'Authorization':
+                                                             f"Bearer {create_access_token({'id': 0})}"})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
