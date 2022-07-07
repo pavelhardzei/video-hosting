5a39b103e398d8ad60bf169907d735c9ef852e9b
pavelhardzei <pavelgordei11@gmail.com>
2022-07-07 09:33:51 +0300

    moved access token decoding to utils

--- services/api/auth/dependencies.py
+++ services/api/auth/dependencies.py
@@ -1,20 +1,15 @@
+from auth import utils
 from auth.models import UserProfile
 from base.database.dependencies import session_dependency
-from base.settings import settings
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/v1/auth/signin/')
 
 
 def current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(session_dependency)):
-    try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
-    except JWTError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail=f'{e}')
+    payload = utils.decode_access_token(token)
 
     user_id = payload.get('id')
     user = session.query(UserProfile).filter(UserProfile.id == user_id).first()
