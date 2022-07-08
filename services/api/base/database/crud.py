d3bdf0846708c24e77effc690ed8303d69dcc09c
pavelhardzei <pavelgordei11@gmail.com>
2022-07-08 13:27:15 +0300

    moved IntegrityError handling to db mixin

--- services/api/base/database/crud.py
+++ services/api/base/database/crud.py
@@ -1,14 +1,8 @@
 from base.database.mixins import SaveDeleteDBMixin
-from fastapi import HTTPException, status
 from pydantic import BaseModel
-from sqlalchemy.exc import IntegrityError
 
 
 def update(instance: SaveDeleteDBMixin, data: BaseModel):
     for key, value in data.dict(exclude_unset=True).items():
         setattr(instance, key, value)
-    try:
-        instance.save()
-    except IntegrityError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f'{e.orig}')
+    instance.save()
