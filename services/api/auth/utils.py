fc2bcc1d686fe20e4082eb311c7a9b81dfffc35a
pavelhardzei <pavelgordei11@gmail.com>
2022-07-12 12:27:28 +0300

    added kwargs to decode_access_token

--- services/api/auth/utils.py
+++ services/api/auth/utils.py
@@ -18,9 +18,9 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def decode_access_token(token: str):
+def decode_access_token(token: str, **kwargs):
     try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm], **kwargs)
     except JWTError as e:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail=f'{e}')
