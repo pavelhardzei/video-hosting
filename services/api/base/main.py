7c639281c4ff9b7817e75ed959ae38c3f84b85ea
pavelhardzei <pavelgordei11@gmail.com>
2022-07-18 20:45:17 +0300

    added error codes

--- services/api/base/main.py
+++ services/api/base/main.py
@@ -1,5 +1,8 @@
 from auth.routers import router as auth_router
+from base.schemas.enums import ErrorCodeEnum
 from fastapi import APIRouter, FastAPI
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -10,3 +13,11 @@ router = APIRouter(
 router.include_router(auth_router)
 
 app.include_router(router)
+
+
+@app.exception_handler(StarletteHTTPException)
+def http_exception_handler(request, e):
+    return JSONResponse(status_code=e.status_code,
+                        content={'detail': e.detail,
+                                 'error_code': e.headers.get('Error-Code', ErrorCodeEnum.base_error)
+                                 if e.headers else ErrorCodeEnum.base_error})
