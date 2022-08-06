f4045e96b9d7a121e5ef7e3a2f65a002a9bbad60
pavelhardzei <pavelgordei11@gmail.com>
2022-08-06 11:49:00 +0300

    modified imports to reduce space

--- services/api/auth/routers.py
+++ services/api/auth/routers.py
@@ -1,12 +1,9 @@
 from datetime import datetime
 
-from auth import exceptions, utils
+from auth import exceptions, permissions, utils
 from auth.models import UserProfile, UserSecurity
-from auth.permissions import (UserAccessTokenValid, UserActive, UserEmailNotVerified, UserEmailReady,
-                              UserSecondaryTokenValid)
+from auth.schemas import schemas
 from auth.schemas.enums import EmailTypeEnum
-from auth.schemas.schemas import (AccessTokenSchema, DetailSchema, EmailSchema, TokenSchema, UserPasswordUpdateSchema,
-                                  UserProfileCreateSchema, UserTokenSchema)
 from auth.users.routers import router as users_router
 from base.database.dependencies import session_dependency
 from base.permissions import check_permissions
@@ -22,8 +19,8 @@ router = APIRouter(
 router.include_router(users_router)
 
 
-@router.post('/signup/', response_model=UserTokenSchema, status_code=status.HTTP_201_CREATED)
-def signup(data: UserProfileCreateSchema, background_tasks: BackgroundTasks):
+@router.post('/signup/', response_model=schemas.UserTokenSchema, status_code=status.HTTP_201_CREATED)
+def signup(data: schemas.UserProfileCreateSchema, background_tasks: BackgroundTasks):
     user = UserProfile(**data.dict())
     user.set_password(data.password)
 
@@ -37,12 +34,12 @@ def signup(data: UserProfileCreateSchema, background_tasks: BackgroundTasks):
     return {'access_token': None, 'user': user}
 
 
-@router.post('/email-verification/', response_model=DetailSchema)
-def email_verification(data: TokenSchema, session: Session = Depends(session_dependency)):
+@router.post('/email-verification/', response_model=schemas.DetailSchema)
+def email_verification(data: schemas.TokenSchema, session: Session = Depends(session_dependency)):
     payload = utils.decode_token(data.token)
 
     user = session.get(UserProfile, payload.get('id'))
-    check_permissions(user, (UserEmailNotVerified(), UserSecondaryTokenValid(data.token)))
+    check_permissions(user, (permissions.UserEmailNotVerified(), permissions.UserSecondaryTokenValid(data.token)))
 
     user.is_active = True
     user.security.secondary_token = None
@@ -51,13 +48,13 @@ def email_verification(data: TokenSchema, session: Session = Depends(session_dep
     return {'detail': 'Email successfully verified'}
 
 
-@router.put('/change-password/', response_model=DetailSchema)
-def change_password(data: UserPasswordUpdateSchema, background_tasks: BackgroundTasks,
+@router.put('/change-password/', response_model=schemas.DetailSchema)
+def change_password(data: schemas.UserPasswordUpdateSchema, background_tasks: BackgroundTasks,
                     session: Session = Depends(session_dependency)):
     payload = utils.decode_token(data.token)
 
     user = session.get(UserProfile, payload.get('id'))
-    check_permissions(user, (UserActive(), UserSecondaryTokenValid(data.token)))
+    check_permissions(user, (permissions.UserActive(), permissions.UserSecondaryTokenValid(data.token)))
 
     user.set_password(data.new_password)
     user.security.secondary_token = None
@@ -69,14 +66,14 @@ def change_password(data: UserPasswordUpdateSchema, background_tasks: Background
     return {'detail': 'Password changed'}
 
 
-@router.post('/email-confirmation/', response_model=DetailSchema)
-def email_confirmation(background_tasks: BackgroundTasks, data: EmailSchema,
+@router.post('/email-confirmation/', response_model=schemas.DetailSchema)
+def email_confirmation(background_tasks: BackgroundTasks, data: schemas.EmailSchema,
                        session: Session = Depends(session_dependency)):
     user = session.query(UserProfile).filter(UserProfile.email == data.email).first()
 
     if data.email_type == EmailTypeEnum.verification:
-        check_permissions(user, (UserEmailNotVerified(), ))
-    check_permissions(user, (UserEmailReady(), ))
+        check_permissions(user, (permissions.UserEmailNotVerified(), ))
+    check_permissions(user, (permissions.UserEmailReady(), ))
 
     user.security.email_sent_time = datetime.utcnow()
     user.security.secondary_token = utils.create_token({'id': user.id})
@@ -88,14 +85,14 @@ def email_confirmation(background_tasks: BackgroundTasks, data: EmailSchema,
     return {'detail': 'Email sent'}
 
 
-@router.post('/signin/', response_model=UserTokenSchema)
+@router.post('/signin/', response_model=schemas.UserTokenSchema)
 def signin(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(session_dependency)):
     user = session.query(UserProfile).filter(UserProfile.email == form_data.username).first()
 
     if user is None or not user.check_password(form_data.password):
         raise exceptions.InvalidCredentialsException()
 
-    check_permissions(user, (UserActive(), ))
+    check_permissions(user, (permissions.UserActive(), ))
 
     user.security.access_token = utils.create_token({'id': user.id})
     user.save()
@@ -103,12 +100,12 @@ def signin(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = 
     return {'access_token': user.security.access_token, 'user': user}
 
 
-@router.post('/refresh-token/', response_model=AccessTokenSchema)
-def refresh_token(data: AccessTokenSchema, session: Session = Depends(session_dependency)):
+@router.post('/refresh-token/', response_model=schemas.AccessTokenSchema)
+def refresh_token(data: schemas.AccessTokenSchema, session: Session = Depends(session_dependency)):
     payload = utils.decode_token(data.access_token, options={'verify_exp': False})
 
     user = session.get(UserProfile, payload.get('id'))
-    check_permissions(user, (UserActive(), UserAccessTokenValid(data.access_token)))
+    check_permissions(user, (permissions.UserActive(), permissions.UserAccessTokenValid(data.access_token)))
 
     user.security.access_token = utils.create_token({'id': user.id})
     user.save()
